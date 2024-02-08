f112f68d6f1a08e27d977c9ff2f8a2dbd542201a
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-02-08 20:00:24 +0530

    Added Analytics"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import QRCodeGenerator from './QRCodeGenerator';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <h1>QR Code Generator</h1>
       <QRCodeGenerator />
+      <Analytics />
     </div>
   );
 }

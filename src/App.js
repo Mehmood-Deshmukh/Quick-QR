e1a29863a6324b37f4c3e0a4a4d99429e9efd5bd
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-02-10 11:56:05 +0530

    Disabled Analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import QRCodeGenerator from './QRCodeGenerator';
-import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <h1>QR Code Generator</h1>
       <QRCodeGenerator />
-      <Analytics />
     </div>
   );
 }
